80f8d242dc355f60327861f85d2929da36cf1546
Piumi <piumiekanayaka4@gmail.com>
2022-05-12 00:54:30 +0530

    fixed errors

---
+++ src/models/Category.ts
@@ -0,0 +1,31 @@
+import {ICategory} from "../interfaces/ICategory";
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const CategorySchema = new Schema(
+    {
+        name:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        description:{
+            type:String,
+            required:false,
+            trim:true,
+        },
+        products:[{type:Schema.Types.ObjectId,required:false,ref:'products'}],
+        image:{
+            type:String,
+            required:true,
+        },
+        status:{
+            type:String,
+            default:"active",
+
+        }
+    }
+
+
+
+);
+export default mongoose.model<ICategory & mongoose.Document>('category',CategorySchema)
